de4fe8fda98a31e168a7e9546914346b5c03c702
margotfilleton <margot.filleton@gmail.com>
2017-02-20 15:06:20 +0000

    Config material UI and Top bar update

--- src/Components/map.js
+++ src/Components/map.js
@@ -4,24 +4,53 @@ import './map.css';
 
 class gmaps extends Component {
 
+    addPointToMap(lat,lng,value){
+        this.heatmapData = [];
 
+        var latLng = new window.google.maps.LatLng(lat,lng);
+        this.heatmapData.push(latLng);
+        var color = [];
+        if(value == 1){
+            color = [
+                'rgba(255, 0, 0, 0)',
+                'rgba(255, 0, 0, 1)'
+            ];
+        }
+        else if( value == 2)
+        {
+            color = [
+                'rgba(255, 100, 10, 0)',
+                'rgba(255, 100, 10, 1)'
+            ];
+        }
+        else
+        {
+            color = [
+                'rgba(0, 255, 0, 0)',
+                'rgba(0, 255, 0, 1)'
+            ];
+        }
+        this.heatmap = new window.google.maps.visualization.HeatmapLayer({
+            data: this.heatmapData,
+            dissipating: true,
+            map: this.map,
+            gradient: color,
+            radius:100
+        });
+    }
 
 
     componentDidMount() {
         const {lat, lng} = this.props.initialPosition;
         this.map = new window.google.maps.Map(this.refs.map, {
             center: {lat, lng},
-            zoom: 16,
-            mapTypeId: 'terrain'
+            zoom: 16
         });
-
-      /*  this.marker = new window.google.maps.Marker({
-            position: {lat, lng},
-            map: this.map,
-            title: 'Hello World!'
-        });*/
-
-
+        this.addPointToMap(51.5248541,-0.08040660000006028,3);
+        this.addPointToMap(51.5258541,-0.08040660000006028,2);
+        this.addPointToMap(51.5268541,-0.08331660000006028,3);
+        this.addPointToMap(51.5268541,-0.08222760000006028,3);
+        this.addPointToMap(51.5268541,-0.08113860000006028,3);
 
     }
     render() {
